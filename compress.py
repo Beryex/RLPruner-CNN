249b6a1026896622c8615f456b52d3ef825de209
王伯垚 <wangboyao@25.local>
2024-06-21 14:44:43 +0800

    [Regular] Fix resume

--- compress.py
+++ compress.py
@@ -330,6 +330,7 @@ if __name__ == '__main__':
         net = torch.load(f'models/{net_name}_{dataset_name}_{args.resume_id}_temp.pth').to(device)
         net_class = get_net_class(net=net_name)
         teacher_id = prev_checkpoint['teacher_id']
+        teacher_id = 1718771151
         teacher_net = torch.load(f'models/{net_name}_{dataset_name}_{teacher_id}_original.pth').to(device)
         train_loader, valid_loader, test_loader, _, _ = get_dataloader(dataset=dataset_name, pin_memory=True)
     else:
@@ -348,7 +349,7 @@ if __name__ == '__main__':
         net = torch.load(f'models/{net_name}_{dataset_name}_{args.net_id}_original.pth').to(device)
         net_class = get_net_class(net=net_name)
         teacher_net = copy.deepcopy(net).to(device)
-        teacher_id = random_seed
+        teacher_id = args.net_id
         train_loader, valid_loader, test_loader, _, _ = get_dataloader(dataset=dataset_name, pin_memory=True)
 
     # initialize parameter to compute complexity of model
